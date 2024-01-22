8680a3cec063bfb7fc15628236824828fe207ac3
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2024-01-22 22:43:23 +0530

    update chapter 1

--- Chapter-1/App.js
+++ Chapter-1/App.js
@@ -5,12 +5,18 @@ const div = React.createElement(
     React.createElement("h1", { id: "h1Tag" }, "This is h1 tag"),
     React.createElement("h2", { id: "h2Tag" }, "This is h2 tag"),
     React.createElement("p", { id: "pTag" }, "Hello World from React 🌎🌎"),
+    React.createElement(
+      "span",
+      { id: "pTag" },
+      "Hello World from React 🌎🌎.This is span"
+    ),
   ],
   ""
 );
 // Always remember whenever you are creating element using React.createElement() it just creates an object and while rendering it is converted into following element.
 // creates a JS Object..
-// console.log(div);
+// The above code will get complicated if nesting is going to get increase.
+console.log(div);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
+// console.log(root);
 root.render(div);
